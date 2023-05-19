256ffd6c459f488b4e5368fe9c95d9263343a9d9
Ateeq44 <Ateeqrehman4479@gmail.com>
2023-05-19 07:16:40 -0700

    gdf

--- resources/views/criminals/view.blade.php
+++ resources/views/criminals/view.blade.php
@@ -1,4 +1,64 @@
+<style>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
 
+.switch input { 
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
 @extends('layout.app')
 <title>
     Criminal
@@ -663,6 +723,14 @@
 
                 <form action="{{ url('Property') }}" method="POST">
                     @csrf
+
+                    <label class="switch">
+                      <input type="checkbox" id="toggleButton"  checked>
+                      <span class="slider"></span>
+                    </label>
+
+                    <div id="div1">Div 1</div>
+                    <div id="div2" style="display: none;">Div 2</div>
                     <div class="form-outline mb-4">
                         <label class="form-label" >Type</label>
                         <input type="text" name="type" class="form-control">
@@ -691,5 +759,22 @@
         </div>
     </div>
 </div>
+
+@endsection
+@section('script')
+
+    const toggleButton = document.getElementById('toggleButton');
+    const div1 = document.getElementById('div1');
+    const div2 = document.getElementById('div2');
+
+    toggleButton.addEventListener('change', function() {
+        if (this.checked) {
+            div1.style.display = 'none';
+            div2.style.display = 'block';
+        } else {
+            div1.style.display = 'block';
+            div2.style.display = 'none';
+        }
+    });
 
 @endsection
